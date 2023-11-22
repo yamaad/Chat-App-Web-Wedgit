9ff60a600100a5a138d9a2226ab6fbf1994358a8
yamaad <maad@quickdesk.io>
2023-11-22 14:09:17 +0800

    APIs setup

--- server/index.js
+++ server/index.js
@@ -1,37 +1,18 @@
 const express = require("express");
 require("dotenv").config();
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
 
 const app = express();
 
-// cors middleware for socket io
-app.use(cors());
-
-//
-const server = http.createServer(app);
-
-//
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
+//  middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-io.on("connection", (socket) => {
-  console.log(`socket connection initiated ${socket.id}`);
 
-  socket.on("send_message", (data) => {
-    socket.broadcast.emit("receive_message", data);
-  });
-  socket.on("disconnect", () => {
-    console.log(`disconnected ${socket.id}`);
-  });
-});
+//routes
 
 // listener
-server.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log("listening on port to server");
 });
